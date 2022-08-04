4481f839ce7ba9ae68433c4bfb7989ded55429c8
ks40698 <110074646+ks40698@users.noreply.github.com>
2022-08-04 20:16:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-add-apt-repository -y ppa:qbittorrent-team/qbittorrent-stable && sudo yum install -y qbittorrent-nox && python3 update.py && python3 -m bot
+sudo apt install -y qbittorrent-nox && python3 update.py && python3 -m bot
